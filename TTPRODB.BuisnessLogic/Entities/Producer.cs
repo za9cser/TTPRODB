c952ad95ebc9873f7908484c7796f1620b1a2726
ZaitsevSergei <zaitsev.sergey.93@gmail.com>
2018-02-12 14:55:41 +0300

    entities created

---
+++ TTPRODB.BuisnessLogic/Entities/Producer.cs
@@ -0,0 +1,16 @@
+﻿namespace TTPRODB.BuisnessLogic.Entities
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Producer() { }
+
+        public Producer(int id, string name)
+        {
+            Id = id;
+            Name = name;        
+        }
+    }
+}

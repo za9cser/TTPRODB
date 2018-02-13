f5081478f6f007c90a71a865d0805466e2ac0724
ZaitsevSergei <zaitsev.sergey.93@gmail.com>
2018-02-13 16:06:58 +0300

    Created a DbConnect class to connect to db

---
+++ TTPRODB.DatabaseCommunication/DbConnect.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TTPRODB.DatabaseCommunication
+{
+    public class DbConnect
+    {
+        private string dbConnectionString;
+        public SqlConnection DbConnection;
+        private string dbName;
+
+        public DbConnect()
+        {
+            // init database's conection parameters  
+            dbName = "TTPRODB";
+            dbConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename =|DataDirectory|\TTPRODB.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            ConnecToDatabase();
+        }
+
+        // connect to database
+        public void ConnecToDatabase()
+        {
+            // init mdf file parameters
+            string outputFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string mdfFilename = dbName + ".mdf";
+            string dbFileName = Path.Combine(outputFolder, mdfFilename);
+
+            // If the file doesn't exists, create it
+            if (!File.Exists(dbFileName))
+            { 
+                CreateDatabase(dbFileName);
+            }
+        }
+
+        // create local database
+        public void CreateDatabase(string dbFileName)
+        {
+            try
+            {
+                // connect to localdb and create database
+                const string connectionString =
+                    @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var cmd = connection.CreateCommand())
+                    {
+                        // detach database
+                        DetachDatabase(connection);
+                        // create new database
+                        cmd.CommandText =
+                            $"CREATE DATABASE {dbName} ON (NAME = N'{dbName}', FILENAME = '{dbFileName}')";
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                // if database created, open connection to it and create structure
+                // load and execute sql script to create database's structure and add foreign keys
+                ExecuteQuery(File.ReadAllText(@"Scripts\CreateDbStructure.sql"),
+                             File.ReadAllText(@"Scripts\\AddForeignKeys.sql"));
+            }
+            catch (SqlException)
+            {
+                
+            }
+        }
+
+        // detach database
+        public void DetachDatabase(SqlConnection connection)
+        {
+            try
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = $"exec sp_detach_db '{dbName}'";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException)
+            {
+
+            }
+        }
+
+        // execute Query to database
+        public SqlDataReader[] ExecuteQuery(params string[] queries)
+        {
+            SqlDataReader[] resultSqlDataReaders = new SqlDataReader[queries.Length];
+            using (var connection = new SqlConnection(dbConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        try
+                        {
+                            for (int i = 0; i < queries.Length; i++)
+                            {
+                                try
+                                {
+                                    command.CommandText = queries[i];
+                                    resultSqlDataReaders[i] = command.ExecuteReader();
+                                }
+                                catch (SqlException)
+                                {
+                                }
+                            }
+                        }
+                        catch (NullReferenceException)
+                        {
+                            
+                        }
+                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
+            return resultSqlDataReaders;
+        }
+    }
+}

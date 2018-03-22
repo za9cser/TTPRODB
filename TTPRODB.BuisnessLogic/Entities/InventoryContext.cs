149468a25bb8a07675057b0c8846a2d0a0cfa52c
ZaitsevSergei <zaitsev.sergey.93@gmail.com>
2018-03-22 12:21:26 +0300

    Created InventoryContext

---
+++ TTPRODB.BuisnessLogic/Entities/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTPRODB.BuisnessLogic.Entities
+{
+    public class InventoryContext : DataContext
+    {
+        public InventoryContext(string fileOrServerOrConnection) : base(fileOrServerOrConnection)
+        {
+
+        }
+    }
+}
